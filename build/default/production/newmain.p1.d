74666a1d96c94998a833b6d3759fad525790d11e
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2019-08-11 17:44:48 +1000

    Add files via upload

---
+++ build/default/production/newmain.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/newmain.d  \
+ build/default/production/newmain.p1:  \
+ newmain.c  \
+16F1717_internal.h 
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=C_PIC16F1717_PWM_motor_driver.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/C_PIC16F1717_PWM_motor_driver.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cpic16f1717pwmmotordriver.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cpic16f1717pwmmotordriver.x.tar
